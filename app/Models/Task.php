42b907bba9d6a871d34c07403f343ead5399b684
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2020-02-10 15:26:18 +0100

    Add fillable fields for Tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
+    protected $fillable = ['title', 'description', 'date', 'user_id'];
+
     /**
      * The user that the task belongs to
      *
