bb5499d65ba50c4cf51eda3afba636dc177560c2
Magdalena Siljanoska <magdalena@unlimitedcoders.com>
2020-02-10 14:47:06 +0100

    Add controllers for Tasks and Users

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        // TODO
+    }
+
+    public function store()
+    {
+        // TODO
+    }
+
+    public function show()
+    {
+        // TODO
+    }
+
+    public function update()
+    {
+        // TODO
+    }
+
+    public function delete()
+    {
+        // TODO
+    }
+}
