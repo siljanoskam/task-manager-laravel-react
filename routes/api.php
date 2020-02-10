baca9a19cf7b0cb42ba987b141fe1974d46e426b
Magdalena Siljanoska <magdalena@unlimitedcoders.com>
2020-02-10 14:45:58 +0100

    Add needed routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,20 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('users')->group(function () {
+    Route::get('/', 'Api\UserController@index')->name('users');
+    Route::post('/', 'Api\UserController@store')->name('users.store');
+    Route::get('/{id}', 'Api\UserController@show')->name('users.store');
+    Route::put('/{id}', 'Api\UserController@update')->name('users.store');
+    Route::delete('/{id}', 'Api\UserController@delete')->name('users.delete');
+});
+
+Route::prefix('tasks')->group(function () {
+    Route::get('/', 'Api\TaskController@index')->name('tasks');
+    Route::post('/', 'Api\TaskController@store')->name('tasks.store');
+    Route::get('/{id}', 'Api\TaskController@show')->name('tasks.show');
+    Route::put('/{id}', 'Api\TaskController@update')->name('tasks.update');
+    Route::delete('/{id}', 'Api\TaskController@index')->name('tasks.delete');
 });
