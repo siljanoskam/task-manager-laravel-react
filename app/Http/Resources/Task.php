806b4f5cf7e2122ab5e93f775a3b02e316d2b907
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2020-02-10 17:43:54 +0100

    Add resources for Tasks and Users

---
+++ app/Http/Resources/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Task extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'date' => $this->date,
+            'user_id' => $this->user_id
+        ];
+    }
+}
