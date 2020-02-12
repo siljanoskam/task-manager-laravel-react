20deb339ed0c22296b64be81a942785f2523f98c
Magdalena Siljanoska <magdalenasiljanoska979@gmail.com>
2020-02-12 17:39:28 +0100

    Update webpack and add bootstrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,11 +5,25 @@ const mix = require('laravel-mix');
  | Mix Asset Management
  |--------------------------------------------------------------------------
  |
- | Mix provides a clean, fluent API for defining some Webpack build steps
+ | Mix provides a clean, ±fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+/**
+ * Compile SASS
+ */
+mix.sass('resources/sass/app.scss', 'public/css');
+
+mix.react('resources/js/app.js', 'public/js');
+
+if (mix.inProduction()) {
+  mix.disableNotifications();
+  mix.version();
+  return;
+}
+
+mix.webpackConfig({
+  devtool: 'source-map',
+});
